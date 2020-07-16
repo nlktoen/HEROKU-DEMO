01b5ac8b0d379ca9a63d5224c30ac21c3e49a6a9
Katie Nguyen <nlktoen@users.noreply.github.com>
2020-07-16 19:23:54 +0700

    Update text in app.py

--- app.py
+++ app.py
@@ -27,8 +27,12 @@ def predict():
     data_unseen = pd.DataFrame([final])
     data_unseen_scaled = scaler.transform(data_unseen)
     prediction = model.predict(data_unseen_scaled)
+    if prediction == 0:
+        text="M"
+    elif prediction == 1:
+        text="B"
     cl=max(max((model.predict_proba(data_unseen_scaled))))*100
-    return render_template('index.html',prediction_text='Prediction will be: {}. Confidence level: {}%'.format(prediction,cl))
+    return render_template('index.html',prediction_text='Predicted class will be: {}. Confidence level: {}%'.format(text,cl))
 
 if __name__ == "__main__":
     app.run(debug=True)
